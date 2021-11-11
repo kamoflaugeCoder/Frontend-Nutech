ba9b653f3512d0e324bdbae5db7291b62b63fc24
Veteran <Veteran1@MacBook-Pro.attlocal.net>
2021-11-10 23:40:34 -0500

    second attempt

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import CartScreen from "./screens/CartScreen";
+
+// import Product from './components/Product';
+import ProductScreen from "./screens/ProductScreen";
+import HomeScreen from "./screens/HomeScreen";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="/">
+              <span> Nutech </span> Technologies, LLC
+            </a>
+          </div>
+          <div>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
+          </div>
+        </header>
+        {/* Routes are create in the main section */}
+        <main>
+          <Route path="/cart/:id?" component={CartScreen}></Route>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
+        </main>
+        <footer className="row center">All right reserved</footer>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
